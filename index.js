a705d2b70c51d76ac62e6ba19ae3be1ad7c2ffa9
yagazierem <adaogbonna44@gmail.com>
2022-04-08 21:06:39 +0100

    My Calculator design

---
+++ index.js
@@ -0,0 +1,24 @@
+let sum = 3 + 2;
+console.log(sum);
+let firstName = "Nmesoma";
+// let greeting = "Hello " + firstName +  " Wecome!"
+// console.log(greeting);
+let lastName = "Ogbonna";
+let note = `Hello ${lastName} Welcome!`;
+console.log(note);
+let y = 10;
+y += 6;
+console.log(y);
+y = 6;
+console.log(++y);
+// challenge starts here//
+let one = 12 % 3;
+console.log(one);
+let two = 12 % 5;
+console.log(two);
+let welcomeNote = "Hello " + " World" + " !";
+console.log(welcomeNote);
+console.log("4" == 4);
+let age = 30;
+let result = (age > 21) && (age < 65);
+console.log(result);
