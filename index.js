8b595e0925a0c59d53e7bc5bc0790d5230962c6a
yagazierem <adaogbonna44@gmail.com>
2022-04-18 07:45:26 +0100

    calculator design

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-let sum = 3 + 2;
-console.log(sum);
-let firstName = "Nmesoma";
+// let sum = 3 + 2;
+// console.log(sum);
+// let firstName = "Nmesoma";
 // let greeting = "Hello " + firstName +  " Wecome!"
 // console.log(greeting);
-let lastName = "Ogbonna";
-let note = `Hello ${lastName} Welcome!`;
-console.log(note);
-let y = 10;
-y += 6;
-console.log(y);
-y = 6;
-console.log(++y);
+// let lastName = "Ogbonna";
+// let note = `Hello ${lastName} Welcome!`;
+// console.log(note);
+// let y = 10;
+// y += 6;
+// console.log(y);
+// y = 6;
+// console.log(++y);
 // challenge starts here//
-let one = 12 % 3;
-console.log(one);
-let two = 12 % 5;
-console.log(two);
-let welcomeNote = "Hello " + " World" + " !";
-console.log(welcomeNote);
-console.log("4" == 4);
-let age = 30;
-let result = (age > 21) && (age < 65);
-console.log(result);
+// let one = 12 % 3;
+// console.log(one);
+// let two = 12 % 5;
+// console.log(two);
+// let welcomeNote = "Hello " + " World" + " !";
+// console.log(welcomeNote);
+// console.log("4" == 4);
+// let age = 30;
+// let result = (age > 21) && (age < 65);
+// console.log(result);
+
+let name="yaga";
+let food="afang";
+function favFoodName(x,y) {
+    console.log(`My Name is ${name} and my fave food is ${food}.`)
+}
+favFoodName(name,food)
+
+
+
+
+
 
