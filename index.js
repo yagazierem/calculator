f61d1ecd559f962d263ccc8a88fca4a39bf07c1e
yagazierem <adaogbonna44@gmail.com>
2022-05-25 11:08:26 +0100

    calculatorapp

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-// let sum = 3 + 2;
-// console.log(sum);
-// let firstName = "Nmesoma";
-// let greeting = "Hello " + firstName +  " Wecome!"
-// console.log(greeting);
-// let lastName = "Ogbonna";
-// let note = `Hello ${lastName} Welcome!`;
-// console.log(note);
-// let y = 10;
-// y += 6;
-// console.log(y);
-// y = 6;
-// console.log(++y);
-// challenge starts here//
-// let one = 12 % 3;
-// console.log(one);
-// let two = 12 % 5;
-// console.log(two);
-// let welcomeNote = "Hello " + " World" + " !";
-// console.log(welcomeNote);
-// console.log("4" == 4);
-// let age = 30;
-// let result = (age > 21) && (age < 65);
-// console.log(result);
+const numBtn = document.querySelector('#numB');
+// let displayScreen = document.querySelector('#display-math');
+const keys = document.querySelector(".grid-container");
 
-let name="yaga";
-let food="afang";
-function favFoodName(x,y) {
-    console.log(`My Name is ${name} and my fave food is ${food}.`)
-}
-favFoodName(name,food)
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayNum = numBtn.textContent;
+        if (!action) {
+            numBtn.textContent = keyContent;
+            // console.log('number-key');
+        }
+    }
+})
 
 
+// function displayValue() {
+//     return displayScreen.innerHTML += numBtn.innerHTML;
+// }
 
-
-
-
+// function displayValue() {
+//     document.getElementById("numB").innerHTML += '6';
+// }
